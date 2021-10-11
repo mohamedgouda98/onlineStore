1f586a4501407972533d2ba577bbf52f99b2f343
abdo shrief <abdo.shrief270@gmail.com>
2021-10-11 19:55:58 +0200

    Add Link of Color In Navbar

--- resources/views/admin/assets/navbar.blade.php
+++ resources/views/admin/assets/navbar.blade.php
@@ -389,6 +389,18 @@
                         </a>
                     </li>
                     <li class="menu">
+                        <a href="{{ route('admin.colors.index') }}" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                            <svg xmlns=" http://www.w3.org/2000/svg"
+                                width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="feather feather-message-square">
+                                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
+                                <span>Colors</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="menu">
                         <a href="{{ route('admin.product') }}" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
                             <svg xmlns=" http://www.w3.org/2000/svg"
